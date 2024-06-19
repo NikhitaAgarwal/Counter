14b3f349b1e2c3b975743a6ef205b83997d751ba
Nikhita Agarwal <96190789+NikhitaAgarwal@users.noreply.github.com>
2024-06-19 11:37:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0); // Functions to handle increment,  decrement, and reset
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
+  const incrementButtonStyle = {
+    backgroundColor: count % 2 === 0 ? "#4caf50" : "#f44336", // Green for even, red for odd
+    border: "none",
+    borderRadius: "5px",
+    color: "white",
+    padding: "10px 20px",
+    margin: "0 10px",
+    fontSize: "16px",
+    cursor: "pointer",
+  };
+
+  const decrementButtonStyle = {
+    backgroundColor: count % 2 != 0 ? "#4caf50" : "#f44336", // Green for even, red for odd
+    border: "none",
+    borderRadius: "5px",
+    color: "white",
+    padding: "10px 20px",
+    margin: "0 10px",
+    fontSize: "16px",
+    cursor: "pointer",
+  };
+
+  const Reset = {
+    border: "none",
+    borderRadius: "5px",
+    color: "Black",
+    padding: "10px 20px",
+    margin: "0 10px",
+    fontSize: "16px",
+    cursor: "pointer",
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Hooks Counter</h1>
+        <p>Count: {count}</p>
+        <div className="button-group">
+          <button style={incrementButtonStyle} onClick={increment}>
+            Increment
+          </button>
+          <button style={decrementButtonStyle} onClick={decrement}>
+            Decrement
+          </button>
+          <button style={Reset} onClick={reset}>
+            Reset
+          </button>
+        </div>
       </header>
     </div>
   );
